a8dbbb3bfbab3ae2d6af62cb3b96e91d7ab337cc
James Nguyen <66525324+jamesnguyenn@users.noreply.github.com>
2022-03-04 19:22:09 +0700

    change search bar position

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ let $$ = document.querySelectorAll.bind(document);
 const link = $(".menu-link");
 const sticky = $(".sticky-menu ");
 const icon = $(".menu-sticky-logo");
+const header = $(".header");
 console.log(sticky);
 //Listener
 link.addEventListener("click", () => {
@@ -11,8 +12,15 @@ link.addEventListener("click", () => {
 });
 link.addEventListener("click", () => {
   icon.classList.toggle("menu-sticky-cancel-active");
+  noScroll();
 });
-menu.addEventListener("click", noScroll);
+
+document.addEventListener("click", function (e) {
+  if (!sticky.contains(e.target) && !header.contains(e.target)) {
+    sticky.classList.remove("run-menu");
+    icon.classList.remove("menu-sticky-cancel-active");
+  }
+});
 
 //Function
 
